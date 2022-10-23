e6d11e545c2a76d6132dbeb81bdbd3052a0b7901
Aprígio Luiz de Melo Gusmão <aprigioluiz@gmail.com>
2022-10-23 15:13:47 -0300

    opção do usuário só permite valor numérico.

--- Projeto_Twitter/principal.py
+++ Projeto_Twitter/principal.py
@@ -1,42 +1,57 @@
 from display_sistema import tela_sistema as tela
 from sistema_busca_twitter import projeto_busca_twitter as proj_twt
 
-tela.tela_inicial()
-opcao_entrada = int(input('Informe uma opção: '))
+
+def verificarDigito(opcao_entrada):
+    while not opcao_entrada.isdigit():
+        print("Informe um valor numérico entre 1 e 5")
+        opcao_entrada = input('Informe uma opção: ')
+    else:
+        return opcao_entrada
 
 if __name__ == '__main__':
     rodar_programa = True
     flag_opcao_entrada = []
-    while rodar_programa:
-        if opcao_entrada != 5 and opcao_entrada != 4:
-            flag_opcao_entrada.append(opcao_entrada)
 
-        if opcao_entrada == 1:
+    tela.tela_inicial()
+    opcao_entrada = input('Informe uma opção: ')
+    entrada_usuario = verificarDigito(opcao_entrada)
+    while rodar_programa:
+
+        if entrada_usuario != '5' and entrada_usuario != '4':
+            if int(entrada_usuario) not in flag_opcao_entrada:
+                flag_opcao_entrada.append(int(entrada_usuario))
+
+        if entrada_usuario == '1':
             tela.tela_buscar_data()
             proj_twt.busca_twt_data()
-
             print(" ")
             tela.tela_inicial()
-            opcao_entrada = int(input('Informe uma opção: '))
-        elif opcao_entrada == 2:
+            opcao_entrada = input('Informe uma opção: ')
+            entrada_usuario = verificarDigito(opcao_entrada)
+        elif entrada_usuario == '2':
             tela.tela_buscar_termo()
             proj_twt.busca_twt_termo()
             print(" ")
             tela.tela_inicial()
-            opcao_entrada = int(input('Informe uma opção: '))
-        elif opcao_entrada == 3:
+            opcao_entrada = input('Informe uma opção: ')
+            entrada_usuario = verificarDigito(opcao_entrada)
+        elif entrada_usuario == '3':
             tela.tela_buscar_assunto()
             proj_twt.busca_twt_assunto()
             print(" ")
             tela.tela_inicial()
-            opcao_entrada = int(input('Informe uma opção: '))
-        elif opcao_entrada == 4:
+            opcao_entrada = input('Informe uma opção: ')
+            entrada_usuario = verificarDigito(opcao_entrada)
+        elif entrada_usuario == '4':
             tela.tela_salvar()
             proj_twt.salvar(flag_opcao_entrada)
             print(" ")
             tela.tela_inicial()
-            opcao_entrada = int(input('Informe uma opção: '))
-        elif opcao_entrada == 5:
+            opcao_entrada = input('Informe uma opção: ')
+            entrada_usuario = verificarDigito(opcao_entrada)
+        elif entrada_usuario == '5':
             tela.tela_sair()
             print("Programa finalizado...")
             rodar_programa = False
+
--- Projeto_Twitter/sistema_busca_twitter/projeto_busca_twitter.py
+++ Projeto_Twitter/sistema_busca_twitter/projeto_busca_twitter.py
@@ -1,15 +1,10 @@
 import csv
 import json
-import os
 from colorama import Fore, Style
-
-# diretorio_BD = os.path.abspath('../base_dados/db_tweets_2022.csv')
-# diretorio = "./db_tweets_2022.csv"
 
 
 # FUNÇÃO PARA ABRIR ARQUIVO
 def abrir_arquivo():
-    #diretorio_BD = os.path.abspath('./././././base_dados/db_tweets_2022.csv')
     diretorio_BD = "base_dados/db_tweets_2022.csv"
     with open(diretorio_BD, "r", encoding="UTF-8") as arquivo:
         ler_arquivo = csv.reader(arquivo, delimiter=',', lineterminator='\n')
@@ -68,6 +63,9 @@ dict_geral_twitter = info_twitter()
 # PESQUISA POR DATA
 data_recebida = ""
 tbl_resultado_data = []
+qtd_data = 0
+
+# TABELA DO RESULTADO DA CONSULTA POR data
 def busca_twt_data():
     '''
     Realiza a pesquisa dos assuntos por data.
@@ -76,25 +74,28 @@ def busca_twt_data():
     '''
     global data_recebida
     global tbl_resultado_data
+    global qtd_data
 
     data_recebida = input("Informe a data que deseja pesquisar (dd/mm/aaaa): ")
 
-    # TABELA DO RESULTADO DA CONSULTA POR data
-    qtd_data = 0
+    # TABELA DATA
     tbl_resultado_data.append(dict_geral_twitter[1]['titulo'])
     qtd_data = sum([qtd_data + 1
                     for indice in range(1, len(dict_geral_twitter) + 1)
                     if data_recebida in dict_geral_twitter[indice][f'data_{indice}']])
 
-    [
-        tbl_resultado_data.append([dict_geral_twitter[indice][f'data_{indice}'],
-                                   dict_geral_twitter[indice][f'conteudo_{indice}'],
-                                   dict_geral_twitter[indice][f'assunto_{indice}']])
-                                   for indice in range(1, len(dict_geral_twitter) + 1)
-                                   if data_recebida in dict_geral_twitter[indice][f'data_{indice}']
-    ]
-    # Chama a função que apresenta o resultada da busca na tela do usuário.
-    apresentar_resultado_data(tbl_resultado_data, qtd_data, data_recebida)
+    if qtd_data != 0:
+        [
+            tbl_resultado_data.append([dict_geral_twitter[indice][f'data_{indice}'],
+                                       dict_geral_twitter[indice][f'conteudo_{indice}'],
+                                       dict_geral_twitter[indice][f'assunto_{indice}']])
+                                       for indice in range(1, len(dict_geral_twitter) + 1)
+                                       if data_recebida in dict_geral_twitter[indice][f'data_{indice}']
+        ]
+        # Chama a função que apresenta o resultada da busca na tela do usuário.
+        apresentar_resultado_data(tbl_resultado_data, qtd_data, data_recebida)
+    else:
+        print(f"\nA sua pesquisa retornou: {Fore.LIGHTGREEN_EX}{qtd_data}{Style.RESET_ALL} resultado(s) para a data {Fore.LIGHTGREEN_EX}{data_recebida}{Style.RESET_ALL}")
 
 # DICIONÁRIO DO RESULTADO DA CONSULTA POR data
 def dct_busca_twt_data():
@@ -113,6 +114,7 @@ def dct_busca_twt_data():
 # PESQUISA POR TERMO
 termo_recebido = ''
 tbl_resultado_termo = []
+qtd_termo = 0
 def busca_twt_termo():
     '''
     Realiza a pesquisa dos assuntos por um termo qualquer informado pelo usuário.
@@ -121,27 +123,30 @@ def busca_twt_termo():
     '''
     global termo_recebido
     global tbl_resultado_termo
+    global qtd_termo
 
     termo_recebido = input("Informe uma palavra que deseja pesquisar: ")
 
     # TABELA DO RESULTADA DA CONSULTA POR termo
     tbl_resultado_termo.append(dict_geral_twitter[1]['titulo'])
-    qtd_termo = 0
+
     qtd_termo = sum([qtd_termo + 1
                      for indice in range(1, len(dict_geral_twitter) + 1)
                      if termo_recebido in dict_geral_twitter[indice][f'conteudo_{indice}'] or
                      termo_recebido in dict_geral_twitter[indice][f'assunto_{indice}']])
-
-    [
-        tbl_resultado_termo.append([dict_geral_twitter[indice][f'data_{indice}'],
-                                    dict_geral_twitter[indice][f'conteudo_{indice}'],
-                                    dict_geral_twitter[indice][f'assunto_{indice}']])
-                                    for indice in range(1, len(dict_geral_twitter) + 1)
-                                    if termo_recebido in dict_geral_twitter[indice][f'conteudo_{indice}' or
-                                       termo_recebido in dict_geral_twitter[indice][f'assunto_{indice}']]
-    ]
-    # Chama a função que apresenta o resultada da busca na tela do usuário.
-    apresentar_resultado_termo(tbl_resultado_termo,qtd_termo,termo_recebido)
+    if qtd_termo != 0:
+        [
+            tbl_resultado_termo.append([dict_geral_twitter[indice][f'data_{indice}'],
+                                        dict_geral_twitter[indice][f'conteudo_{indice}'],
+                                        dict_geral_twitter[indice][f'assunto_{indice}']])
+                                        for indice in range(1, len(dict_geral_twitter) + 1)
+                                        if termo_recebido in dict_geral_twitter[indice][f'conteudo_{indice}' or
+                                           termo_recebido in dict_geral_twitter[indice][f'assunto_{indice}']]
+        ]
+        # Chama a função que apresenta o resultada da busca na tela do usuário.
+        apresentar_resultado_termo(tbl_resultado_termo,qtd_termo,termo_recebido)
+    else:
+        print(f"\nA sua pesquisa retornou: {Fore.LIGHTGREEN_EX}{qtd_termo}{Style.RESET_ALL} resultado(s) para o termo {Fore.LIGHTGREEN_EX}{termo_recebido}{Style.RESET_ALL}.")
 
 # DICIONÁRIO DO RESULTADO DA CONSULTA POR termo
 def dct_busca_twt_termo():
@@ -161,6 +166,7 @@ def dct_busca_twt_termo():
 # PESQUISA POR ASSUNTO
 assunto_recebido = ''
 tbl_resultado_assunto = []
+qtd_assunto = 0
 def busca_twt_assunto():
     '''
     Realiza a pesquisa do conteúdo por assunto.
@@ -169,24 +175,28 @@ def busca_twt_assunto():
     '''
     global assunto_recebido
     global tbl_resultado_assunto
+    global qtd_assunto
 
     assunto_recebido = input("Informe uma palavra que deseja pesquisar o assunto: ")
 
     # TABELA DO RESULTADA DA CONSULTA POR termo
     tbl_resultado_assunto.append(dict_geral_twitter[1]['titulo'])
-    qtd_assunto = 0
+
     qtd_assunto = sum([qtd_assunto + 1
                        for indice in range(1, len(dict_geral_twitter) + 1)
                        if assunto_recebido in dict_geral_twitter[indice][f'assunto_{indice}']])
 
-    [
-        tbl_resultado_assunto.append([dict_geral_twitter[indice][f'data_{indice}'],
-                                      dict_geral_twitter[indice][f'conteudo_{indice}'],
-                                      dict_geral_twitter[indice][f'assunto_{indice}']])
-                                      for indice in range(1, len(dict_geral_twitter) + 1)
-                                      if assunto_recebido in dict_geral_twitter[indice][f'assunto_{indice}']
-    ]
-    apresentar_resultado_assunto(tbl_resultado_assunto,qtd_assunto,assunto_recebido)
+    if qtd_assunto != 0:
+        [
+            tbl_resultado_assunto.append([dict_geral_twitter[indice][f'data_{indice}'],
+                                          dict_geral_twitter[indice][f'conteudo_{indice}'],
+                                          dict_geral_twitter[indice][f'assunto_{indice}']])
+                                          for indice in range(1, len(dict_geral_twitter) + 1)
+                                          if assunto_recebido in dict_geral_twitter[indice][f'assunto_{indice}']
+        ]
+        apresentar_resultado_assunto(tbl_resultado_assunto,qtd_assunto,assunto_recebido)
+    else:
+        print(f"\nA sua pesquisa retornou: {Fore.LIGHTGREEN_EX}{qtd_termo}{Style.RESET_ALL} resultado(s) para o assunto {Fore.LIGHTGREEN_EX}{termo_recebido}{Style.RESET_ALL}.")
 
 # DICIONÁRIO DO RESULTADO DA CONSULTA POR assunto
 def dct_busca_twt_assunto():
@@ -204,78 +214,143 @@ def dct_busca_twt_assunto():
 
 
                                     #####################################################
-
 #####################################             FUNÇÃO PARA SALVAR ARQUIVO            ################################
                                     #####################################################
 
 # FUNÇÃO PARA SALVAR
 def salvar(flag_opcao_entrada):
-
+    arquivo_salvo = []
     # chama a função flag_opcao_entrada para identificar as buscas a serem salvas
-    contador = 0
+    contador_busca = 0
+    dict_mensagem = {}
     for opcao in flag_opcao_entrada:
-        contador+=1
+        contador_busca += 1
+
         # SALVAR pesquisa por data
         if opcao == 1:
-            dict_twitter = dct_busca_twt_data()
+            if qtd_data == 0:
+                mensagem_data = ("FILTRO POR DATA: Não existem dados a serem salvos!")
+                arquivo_salvo.append(False)
+                dict_mensagem.update({'data':{False:mensagem_data}})
+            else:
+                dict_twitter = dct_busca_twt_data()
 
-            # FORMATO JSON
-            # dicionário para JSON
-            arquivo_json_data = json.dumps(dict_twitter)
-            # escrevendo JSON em arquivo
-            with open("arquivos_salvos/twt_resultado_data.json", "w", encoding='UTF-8') as resultado_data_json:
-                json.dump(arquivo_json_data, resultado_data_json)
+                # FORMATO JSON
+                # dicionário para JSON
+                arquivo_json_data = json.dumps(dict_twitter)
+                # escrevendo JSON em arquivo
+                with open("arquivos_salvos/twt_resultado_data.json", "w", encoding='UTF-8') as resultado_data_json:
+                    json.dump(arquivo_json_data, resultado_data_json)
 
-            # FORMATO CSV
-            tbl_resultado_data
-            with open('arquivos_salvos/resultado_data.csv', 'w', encoding='UTF-8') as arquivo_csv:
-                escrever_csv = csv.writer(arquivo_csv,
-                                          delimiter='|',
-                                          lineterminator='\n')
-                escrever_csv.writerows(tbl_resultado_data)
+                # FORMATO CSV
+                tbl_resultado_data
+                with open('arquivos_salvos/resultado_data.csv', 'w', encoding='UTF-8') as arquivo_csv:
+                    escrever_csv = csv.writer(arquivo_csv,
+                                              delimiter='|',
+                                              lineterminator='\n')
+                    escrever_csv.writerows(tbl_resultado_data)
+                mensagem_data = "FILTRO POR DATA: gravando arquivo..."
+                arquivo_salvo.append(True)
+                dict_mensagem.update({'data': {True: mensagem_data}})
 
         # SALVAR pesquisa por termo
         if opcao == 2:
-            dict_twitter = dct_busca_twt_termo()
+            if qtd_termo == 0:
+                print("FILTRO POR TERMO: Não existem dados a serem salvos!")
+                arquivo_salvo.append(False)
+                dict_mensagem.update({'termo': {False: mensagem_data}})
+            else:
+                dict_twitter = dct_busca_twt_termo()
 
-            # FORMATO JSON
-            # dicionário para JSON
-            arquivo_json_termo = json.dumps(dict_twitter)
-            # escrevendo JSON em arquivo
-            with open("arquivos_salvos/twt_resultado_termo.json", "w",encoding='UTF-8') as resultado_termo_json:
-                json.dump(arquivo_json_termo, resultado_termo_json)
+                # FORMATO JSON
+                # dicionário para JSON
+                arquivo_json_termo = json.dumps(dict_twitter)
+                # escrevendo JSON em arquivo
+                with open("arquivos_salvos/twt_resultado_termo.json", "w",encoding='UTF-8') as resultado_termo_json:
+                    json.dump(arquivo_json_termo, resultado_termo_json)
 
-            # FORMATO CSV
-            tbl_resultado_termo
-            with open('arquivos_salvos/resultado_termo.csv', 'w', encoding='UTF-8') as arquivo_csv:
-                escrever_csv = csv.writer(arquivo_csv,
-                                          delimiter='|',
-                                          lineterminator='\n')
-                escrever_csv.writerows(tbl_resultado_termo)
+                # FORMATO CSV
+                tbl_resultado_termo
+                with open('arquivos_salvos/resultado_termo.csv', 'w', encoding='UTF-8') as arquivo_csv:
+                    escrever_csv = csv.writer(arquivo_csv,
+                                              delimiter='|',
+                                              lineterminator='\n')
+                    escrever_csv.writerows(tbl_resultado_termo)
+                print("FILTRO POR TERMO: gravando arquivo...")
+                arquivo_salvo.append(True)
+                dict_mensagem.update({'termo': {True: mensagem_data}})
 
         # SALVAR pesquisa por assunto
         if opcao == 3:
-            dict_twitter = dct_busca_twt_assunto()
+            if qtd_assunto == 0:
+                print("FILTRO POR ASSUNTO: Não existem dados a serem salvos!")
+                arquivo_salvo.append(False)
+                dict_mensagem.update({'assunto': {False: mensagem_data}})
+            else:
+                dict_twitter = dct_busca_twt_assunto()
 
-            # FORMATO JSON
-            # dicionário para JSON
-            arquivo_json_assunto = json.dumps(dict_twitter)
-            # escrevendo JSON em arquivo
-            with open("arquivos_salvos/twt_resultado_assunto.json", "w", encoding='UTF-8') as resultado_assunto_json:
-                json.dump(arquivo_json_assunto, resultado_assunto_json)
+                # FORMATO JSON
+                # dicionário para JSON
+                arquivo_json_assunto = json.dumps(dict_twitter)
+                # escrevendo JSON em arquivo
+                with open("arquivos_salvos/twt_resultado_assunto.json", "w", encoding='UTF-8') as resultado_assunto_json:
+                    json.dump(arquivo_json_assunto, resultado_assunto_json)
 
-            # FORMATO CSV
-            tbl_resultado_assunto
-            with open('arquivos_salvos/resultado_assunto.csv', 'w', encoding='UTF-8') as arquivo_csv:
-                escrever_csv = csv.writer(arquivo_csv,
-                                          delimiter='|',
-                                          lineterminator='\n')
-                escrever_csv.writerows(tbl_resultado_assunto)
+                # FORMATO CSV
+                tbl_resultado_assunto
+                with open('arquivos_salvos/resultado_assunto.csv', 'w', encoding='UTF-8') as arquivo_csv:
+                    escrever_csv = csv.writer(arquivo_csv,
+                                              delimiter='|',
+                                              lineterminator='\n')
+                    escrever_csv.writerows(tbl_resultado_assunto)
+                print("FILTRO POR ASSUNTO: gravando arquivo...")
+                arquivo_salvo.append(True)
+                dict_mensagem.update({'assunto': {True: mensagem_data}})
 
-    if contador == 0:
+    cont_false = 0
+    cont_arquivo_salvo = 0
+    msg_busca_salva = ''
+    # MENSAGEM A SER EXIBIDA
+    for chave,valor in dict_mensagem.items():
+        # DATA
+        if chave == 'data':
+            for chave_bool in dict_mensagem[chave]:
+                if chave_bool == False:
+                    msg_busca_salva = dict_mensagem['data'][False]
+                    cont_false+=1
+                else:
+                    msg_busca_salva = dict_mensagem['data'][True]
+                    cont_arquivo_salvo+=1
+        # TERMO
+        if chave == 'termo':
+            for chave_bool in dict_mensagem[chave]:
+                if chave_bool == False:
+                    msg_busca_salva = dict_mensagem['termo'][False]
+                    cont_false+=1
+                else:
+                    msg_busca_salva = dict_mensagem['termo'][True]
+                    cont_arquivo_salvo+=1
+        # ASSUNTO
+        if chave == 'assunto':
+            for chave_bool in dict_mensagem[chave]:
+                if chave_bool == False:
+                    msg_busca_salva = dict_mensagem['assunto'][False]
+                    cont_false+=1
+                else:
+                    msg_busca_salva = dict_mensagem['assunto'][True]
+                    cont_arquivo_salvo+=1
+
+    print(msg_busca_salva)
+
+    if contador_busca == 0:
         print("Não existe busca realizada.")
     else:
-        print("Arquivo salvo com sucesso!")
+        if cont_arquivo_salvo != 0:
+            print("Arquivo(s) salvo(s) com sucesso!")
+        if cont_false == 3:
+            print("Não existem arquivos a serem salvos!")
+
+
 
 
                               ############################################################
